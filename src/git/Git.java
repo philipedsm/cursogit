ca898b4ddf5e214fb6eb46a72ffb239cff4d6c11
Philipe <philipedsm@gmail.com>
2019-05-28 20:05:21 -0300

    Primeiro commit.

---
+++ src/git/Git.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author usuario
+ */
+public class Git {
+
+
+    public static int calcula(int a, int b) {
+        return(a+b);
+    }
+    
+    public static void main(String[] args) {
+
+        int resultado=calcula(1,2);
+        System.err.println("Resultado:" + resultado);
+
+
+    }
+    
+}
