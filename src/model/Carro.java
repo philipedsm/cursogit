89eb159afd0d01a7fb4ecfce5908345af981572e
Philipe <philipedsm@gmail.com>
2019-05-28 21:20:18 -0300

    Charlles falotu a minha colação de grau, e é um desgraçado.

---
+++ src/model/Carro.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author usuario
+ */
+public class Carro {
+
+    private int ano;
+    private String cor;
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+}
---
+++ src/model/CharlesFaltao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author usuario
+ */
+public class CharlesFaltao {
+
+    private String faltouAMinhAColacaodegrau;
+
+    public String getFaltouAMinhAColacaodegrau() {
+        return faltouAMinhAColacaodegrau;
+    }
+
+    public void setFaltouAMinhAColacaodegrau(String faltouAMinhAColacaodegrau) {
+        this.faltouAMinhAColacaodegrau = faltouAMinhAColacaodegrau;
+    }
+
+}
